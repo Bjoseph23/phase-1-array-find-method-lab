7f0681a6ac2fb4ff98f921798190b2bc0e9330de
+ <+>
2024-03-31 21:54:30 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year:"2015", result: "w"},
+    { year:"2014", result: "N/A"},
+    { year:"2013", result: "L"},
+]
+const superbowlWin = (record) => {
+    const winningYear = record.find(
+      (element) => element.result.toLowerCase() === "w"
+    );
+    if (winningYear) {
+      return winningYear.year; 
+    } else {
+      return undefined;
+    }
+  };
